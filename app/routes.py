5f4611b527fc17240ca547a606fd9c6f4d195eeb
Walter Alcantara <waltermalcantara@gmail.com>
2022-08-09 19:47:58 -0300

    templates

--- app/routes.py
+++ app/routes.py
@@ -4,4 +4,15 @@ from app import app
 @app.route('/')
 @app.route('/index')
 def index():
-  return 'Hello, World!'
+  user = {'username': 'Walter'}
+  posts = [
+    {
+      'author': {'username': 'John'},
+      'body': 'Beautiful day in Portland!'
+    },
+    {
+      'author': {'username': 'Susan'},
+      'body': 'The Avengers movie was so cool!'
+    }
+  ]
+  return render_template('index.html', title="Home", user=user, posts=posts)
