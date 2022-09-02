0bc62cff0f930a7acf51a8c9886d7deb9200dabc
Walter Alcantara <waltermalcantara@gmail.com>
2022-09-02 19:43:54 -0300

    facelift

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,11 @@
+import logging
 from flask import Flask
-from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
 from flask_mail import Mail
-import logging
+from flask_bootstrap import Bootstrap
+from config import Config
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -13,6 +14,7 @@ migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = 'login'
 mail = Mail(app)
+bootstrap = Bootstrap(app)
 
 from app import routes, models, errors
 from logging.handlers import SMTPHandler, RotatingFileHandler
